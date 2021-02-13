a0965eb2330e13882c4b9feac796f3db647bdb4f
Carlos A. Ospina <carlosaospinac@gmail.com>
2021-02-13 17:12:58 -0500

    alembic + setup update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+if [ ! -f .env ]; then
+    cp .env.template .env
+fi
+
 docker-compose build
-python3 -m venv web/.venv
-web/.venv/bin/pip install --upgrade pip
+
+if [ ! -d web/.venv ]; then
+    python3 -m venv web/.venv
+    web/.venv/bin/pip install --upgrade pip
+fi
+
 web/.venv/bin/pip install -r web/requirements-dev.txt
 web/.venv/bin/pip install -r web/requirements.txt
 
+ALEMBIC_DIR="web/alem"
+
+if [ ! -d $ALEMBIC_DIR ]; then
+    web/.venv/bin/python -m alembic init $ALEMBIC_DIR
+    eval $(egrep -v '^#' .env | xargs)
+
+    NEW_LINE="sqlalchemy.url = postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD"
+    NEW_LINE="$NEW_LINE@$POSTGRES_SERVER/$POSTGRES_DB"
+
+    sed -i "/sqlalchemy.url/c$NEW_LINE" alembic.ini
+    sed -i '1 i\# pylint: disable=no-member' "$ALEMBIC_DIR/env.py"
+fi
+
 echo "All done."
-echo "Run 'docker-compose up' to start services and finish building"
+echo "Run 'docker-compose up' to start services"
--- web/db/__init__.py
+++ web/db/__init__.py
@@ -1,8 +1,19 @@
-from sqlalchemy import create_engine
+import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 
 
 from lib.const import DB_HOST
 
-engine = create_engine(DB_HOST, echo=True)
-Base = declarative_base()
+engine = sa.create_engine(DB_HOST, echo=True)
+
+metadata = sa.MetaData(
+    naming_convention={
+        'ix': 'ix_%(column_0_label)s',
+        'uq': 'uq_%(table_name)s_%(column_0_name)s',
+        'ck': 'ck_%(table_name)s_%(constraint_name)s',
+        'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+        'pk': 'pk_%(table_name)s'
+    }
+)
+
+Base = declarative_base(metadata=metadata)
