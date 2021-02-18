d0f4178b8477333bcf79c01a1538ac1985769f1c
Carlos A. Ospina <carlosaospinac@gmail.com>
2021-02-17 21:11:20 -0500

    update user model + added enums

--- web/models/user.py
+++ web/models/user.py
@@ -1,18 +1,29 @@
+import enum
+
 import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
 from . import Model
+
+
+class UserStatus(enum.Enum):
+    active = 'active'
+    inactive = 'inactive'
+
+
+class UserGender(enum.Enum):
+    f = 'f'
+    m = 'm'
 
 
 class User(Model):
 
     __tablename__ = 'users'
-    __table_args__ = (
-        sa.UniqueConstraint('organization_id', 'email',
-                            name='_organization_email_uc'),
-    )
     _repr_hide = ['password']
 
     external_id = sa.Column(sa.String, nullable=False)
-    status = sa.Column(sa.Enum('active', 'inactive'),
+    status = sa.Column(sa.Enum(UserStatus),
                        nullable=False, default='inactive')
     email = sa.Column(sa.String, nullable=False)
     password = sa.Column(sa.String, nullable=False)
@@ -20,5 +31,8 @@ class User(Model):
     last_name = sa.Column(sa.String, nullable=False)
     birthdate = sa.Column(sa.DateTime)
     phone_number = sa.Column(sa.String)
-    gender = sa.Column(sa.Enum('f' 'm'))
+    gender = sa.Column(sa.Enum(UserGender))
     avatar = sa.Column(sa.String)
+    role_id = sa.Column(UUID, sa.ForeignKey('roles.id'), nullable=False)
+
+    role = relationship('Role', back_populates='users')
