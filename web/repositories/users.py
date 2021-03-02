f63e392bc89cf95434ceefc51b3886e307b638b3
Carlos A. Ospina <carlosaospinac@gmail.com>
2021-03-01 21:58:34 -0500

    Users repository

--- web/repositories/users.py
+++ web/repositories/users.py
@@ -1,5 +1,12 @@
-from sqlalchemy.orm import Session
 from models import User
+from repositories._utils import Repository, db_access
 
-session = Session()
-query = session.query(User)
+
+class UsersRepository(Repository):
+
+    def get_model(self):
+        return User
+
+    @db_access
+    def get_users(self, skip: int = 0, limit: int = 100):
+        return self.query.offset(skip).limit(limit).all()
