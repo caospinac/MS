c1ef384dc39663101d9e44369c830f20e7085348
Carlos A. Ospina <carlosaospinac@gmail.com>
2021-03-22 16:04:02 -0500

    Health check endpoint

--- web/main.py
+++ web/main.py
@@ -1,20 +1,7 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 from routes import router
 
 
 app = FastAPI()
-
-
-@app.get('/')
-async def _read_root():
-    return {'Hello': 'World'}
-
-
-@app.get('/items/{item_id}')
-async def _read_item(item_id: int, q: Optional[str] = None):
-    return {'item_id': item_id, 'q': q}
-
 app.include_router(router)
--- web/routes/__init__.py
+++ web/routes/__init__.py
@@ -3,6 +3,14 @@ from fastapi import APIRouter
 from .users import router as usersRouter
 from .organizations import router as organizationsRouter
 
+
 router = APIRouter(prefix='/v1')
+
+
+@router.get('/health')
+async def read_root():
+    return 'OK'
+
+
 router.include_router(usersRouter)
 router.include_router(organizationsRouter)
