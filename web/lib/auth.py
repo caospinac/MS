32d76e2adace4afc37ee983daf2cccb019e018b8
Carlos A. Ospina <carlosaospinac@gmail.com>
2021-04-17 20:12:33 -0500

    redis + update auth methods

--- web/lib/auth.py
+++ web/lib/auth.py
@@ -1,24 +1,29 @@
+import uuid
 from datetime import datetime, timedelta
 
 import jwt
 
-from lib.const import ACCESS_TOKEN_KEY, REFRESH_ACCESS_TOKEN_KEY
+from lib.const import (
+    ACCESS_TOKEN_KEY, REFRESH_ACCESS_TOKEN_KEY, TOKEN_EXPIRATION_SEC
+)
 
 
 def generate_token(user_id: str):
-    exp = datetime.utcnow() + timedelta(seconds=30)
+    exp = datetime.utcnow() + timedelta(seconds=TOKEN_EXPIRATION_SEC)
     token = jwt.encode(
         {
+            'sid': uuid.uuid4().hex,
             'uid': user_id,
             'exp': exp,
         },
         ACCESS_TOKEN_KEY,
         algorithm='HS256',
     )
+
     return token, exp
 
 
-def verify(encoded_data: str) -> dict:
+def verify_token(encoded_data: str) -> dict:
     return jwt.decode(encoded_data, ACCESS_TOKEN_KEY, algorithm='HS256')
 
 
@@ -28,3 +33,7 @@ def generate_refresh_token(user_id: str):
         REFRESH_ACCESS_TOKEN_KEY,
         algorithm='HS256'
     )
+
+
+def verify_refresh_token(encoded_data: str) -> dict:
+    return jwt.decode(encoded_data, REFRESH_ACCESS_TOKEN_KEY, algorithm='HS256')
---
+++ web/lib/cache.py
@@ -0,0 +1,34 @@
+import functools
+import json
+
+from redis import Redis
+
+from lib.const import REDIS_HOST
+
+
+client = Redis(host=REDIS_HOST)
+
+
+def save(key: str, payload):
+    client.set(key, json.dumps(payload))
+
+
+def load(key: str):
+    data = client.get(key)
+
+    return data and json.loads(data)
+
+
+def cached(wrapped):
+
+    @functools.wraps(wrapped)
+    def wrapper(*args, cache_key, **kwargs):
+        result = client.get(cache_key)
+
+        if not result:
+            result = wrapped(*args, **kwargs)
+            save(cache_key, result)
+
+        return result
+
+    return wrapper
